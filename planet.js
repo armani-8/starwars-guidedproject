1ff790a29fd6aa052fbffe5314bd456a18944ab9
Alice Smith <alice@smith.com>
2023-06-27 11:32:19 -0400

    updated planets

--- planet.js
+++ planet.js
@@ -1,10 +1,16 @@
 let planetH1;
-let charactersUl;
+let climateU1;
+let terrainU1;
+let populationU1;
 let filmsUl;
+let charactersUl;
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
 
 addEventListener('DOMContentLoaded', () => {
   planetH1 = document.querySelector('h1#title');
+  climateUl = document.querySelector('#climate>ul');
+  terrainUl = document.querySelector('#terrain>ul');
+  populationUl = document.querySelector('#population>ul');
   charactersUl = document.querySelector('#characters>ul');
   filmsUl = document.querySelector('#films>ul');
 
@@ -17,8 +23,8 @@ async function getPlanet(id) {
   let planet;
   try {
     planet = await fetchPlanet(id)
-    planet.characters = await fetchCharacters(planet)
-    planet.films = await fetchFilm(planet)
+    planet.characters = await fetchCharacters(id)
+    planet.films = await fetchFilm(id)
   }
   catch (ex) {
     console.error(`Error reading planet ${id} data.`, ex.message);
@@ -32,29 +38,32 @@ async function fetchPlanet(id) {
     .then(res => res.json())
 }
 
-async function fetchCharacters([planet]) {
-  const url = `${baseUrl}/planets/${planet?.id}/characters`;
+async function fetchCharacters([id]) {
+  const url = `${baseUrl}/planets/${id}/characters`;
   const characters = await fetch(url)
     .then(res => res.json())
   return characters;
 }
 
-async function fetchFilms(planet) {
-  const url = `${baseUrl}/planets/${[planet]?.id}/films`;
+async function fetchFilms(id) {
+  const url = `${baseUrl}/planets/${id}/films`;
   const films = await fetch(url)
     .then(res => res.json())
   return films;
 }
 
 const renderPlanet = planet => {
-  document.title = `SWAPI - ${homeworld?.id}`;  // Just to make the browser tab say the film title
-  titleH1.textContent = homeworld?.id;
-  charactersU1.textContent = homeworld?.characters;
-  filmsU1.textContent = homeworld?.films;
+  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say the film title
+  titleH1.textContent = planet?.name;
+  climateU1.textContent = planet?.climate;
+  terrainU1.textContent = planet?.terrain;
+  populationU1.textContent = planet?.population;
+  charactersU1.textContent = planet?.characters;
+  filmsU1.textContent = planet?.films;
 
-  const characterLis = homeworld?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  const characterLis = planet?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
   charactersUl.innerHTML = characterLis.join("");
 
-  const filmLis = homeworld?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.name}</li>`)
+  const filmLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.name}</li>`)
   filmsUl.innerHTML = filmLis.join("");
 }
